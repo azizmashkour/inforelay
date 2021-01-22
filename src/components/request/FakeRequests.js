e91cab213612cf0aed270a2398875316357367eb
Mashkour Abdel Aziz <azizmashkour@gmail.com>
2021-01-22 21:58:28 +0100

    feat: create fake requests

---
+++ src/components/request/FakeRequests.js
@@ -0,0 +1,509 @@
+// Author https://github.com/yegor-sytnyk/articles-list
+
+export default [
+  {
+    id: 1,
+    title: "Beetlejuice",
+    year: "1988",
+    badge: "Authentic news",
+    shortDescription: "Tim Burton",
+    actors: "Alec Baldwin, Geena Davis, Annie McEnroe, Maurice Page",
+    posterUrl:
+      "https://images-na.ssl-images-amazon.com/images/M/MV5BMTUwODE3MDE0MV5BMl5BanBnXkFtZTgwNTk1MjI4MzE@._V1_SX300.jpg"
+  },
+  {
+    id: 2,
+    title: "The Cotton Club",
+    year: "1984",
+    badge: "Authentic news",
+    shortDescription: "Francis Ford Coppola",
+    actors: "Richard Gere, Gregory Hines, Diane Lane, Lonette McKee",
+    posterUrl:
+      "https://images-na.ssl-images-amazon.com/images/M/MV5BMTU5ODAyNzA4OV5BMl5BanBnXkFtZTcwNzYwNTIzNA@@._V1_SX300.jpg"
+  },
+  {
+    id: 3,
+    title: "The Shawshank Redemption",
+    year: "1994",
+    badge: "Authentic news",
+    shortDescription: "Frank Darabont",
+    actors: "Tim Robbins, Morgan Freeman, Bob Gunton, William Sadler",
+    posterUrl:
+      "https://images-na.ssl-images-amazon.com/images/M/MV5BODU4MjU4NjIwNl5BMl5BanBnXkFtZTgwMDU2MjEyMDE@._V1_SX300.jpg"
+  },
+  {
+    id: 4,
+    title: "Crocodile Dundee",
+    year: "1986",
+    badge: "Authentic news",
+    shortDescription: "Peter Faiman",
+    actors: "Paul Hogan, Linda Kozlowski, John Meillon, David Gulpilil",
+    posterUrl:
+      "https://images-na.ssl-images-amazon.com/images/M/MV5BMTg0MTU1MTg4NF5BMl5BanBnXkFtZTgwMDgzNzYxMTE@._V1_SX300.jpg"
+  },
+  {
+    id: 5,
+    title: "Valkyrie",
+    year: "2008",
+    badge: "Authentic news",
+    shortDescription: "Bryan Singer",
+    actors: "Tom Cruise, Kenneth Branagh, Bill Nighy, Tom Wilkinson",
+    posterUrl:
+      "http://ia.media-imdb.com/images/M/MV5BMTg3Njc2ODEyN15BMl5BanBnXkFtZTcwNTAwMzc3NA@@._V1_SX300.jpg"
+  },
+  {
+    id: 6,
+    title: "Ratatouille",
+    year: "2007",
+    badge: "Authentic news",
+    shortDescription: "Brad Bird, Jan Pinkava",
+    actors: "Patton Oswalt, Ian Holm, Lou Romano, Brian Dennehy",
+    posterUrl:
+      "https://images-na.ssl-images-amazon.com/images/M/MV5BMTMzODU0NTkxMF5BMl5BanBnXkFtZTcwMjQ4MzMzMw@@._V1_SX300.jpg"
+  },
+  {
+    id: 7,
+    title: "City of God",
+    year: "2002",
+    badge: "Authentic news",
+    shortDescription: "Leandro Firmino",
+    actors:
+      "Alexandre Rodrigues, Leandro Firmino, Phellipe Haagensen, Douglas Silva",
+    posterUrl:
+      "https://images-na.ssl-images-amazon.com/images/M/MV5BMjA4ODQ3ODkzNV5BMl5BanBnXkFtZTYwOTc4NDI3._V1_SX300.jpg"
+  },
+  {
+    id: 8,
+    title: "Memento",
+    year: "2000",
+    badge: "Authentic news",
+    shortDescription: "Christopher Nolan",
+    actors: "Guy Pearce, Carrie-Anne Moss, Joe Pantoliano, Mark Boone Junior",
+    posterUrl:
+      "https://images-na.ssl-images-amazon.com/images/M/MV5BNThiYjM3MzktMDg3Yy00ZWQ3LTk3YWEtN2M0YmNmNWEwYTE3XkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg"
+  },
+  {
+    id: 9,
+    title: "The Intouchables",
+    year: "2011",
+    badge: "Authentic news",
+    shortDescription: "Olivier Nakache, Eric Toledano",
+    actors: "François Cluzet, Omar Sy, Anne Le Ny, Audrey Fleurot",
+    posterUrl:
+      "http://ia.media-imdb.com/images/M/MV5BMTYxNDA3MDQwNl5BMl5BanBnXkFtZTcwNTU4Mzc1Nw@@._V1_SX300.jpg"
+  },
+  {
+    id: 10,
+    title: "Stardust",
+    year: "2007",
+    badge: "Authentic news",
+    shortDescription: "Matthew Vaughn",
+    actors: "Ian McKellen, Bimbo Hart, Alastair MacIntosh, David Kelly",
+    posterUrl:
+      "https://images-na.ssl-images-amazon.com/images/M/MV5BMjkyMTE1OTYwNF5BMl5BanBnXkFtZTcwMDIxODYzMw@@._V1_SX300.jpg"
+  },
+  {
+    id: 11,
+    title: "Apocalypto",
+    year: "2006",
+    badge: "Authentic news",
+    shortDescription: "Dalia Hernández",
+    actors:
+      "Rudy Youngblood, Dalia Hernández, Jonathan Brewer, Morris Birdyellowhead",
+    posterUrl:
+      "https://images-na.ssl-images-amazon.com/images/M/MV5BNTM1NjYyNTY5OV5BMl5BanBnXkFtZTcwMjgwNTMzMQ@@._V1_SX300.jpg"
+  },
+  {
+    id: 12,
+    title: "Taxi Driver",
+    year: "1976",
+    badge: "Authentic news",
+    shortDescription: "Martin Scorsese",
+    actors: "Diahnne Abbott, Frank Adu, Victor Argo, Gino Ardito",
+    posterUrl:
+      "https://images-na.ssl-images-amazon.com/images/M/MV5BNGQxNDgzZWQtZTNjNi00M2RkLWExZmEtNmE1NjEyZDEwMzA5XkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg"
+  },
+  {
+    id: 13,
+    title: "No Country for Old Men",
+    year: "2007",
+    badge: "Authentic news",
+    shortDescription: "Ethan Coen, Joel Coen",
+    actors: "Tommy Lee Jones, Javier Bardem, Josh Brolin, Woody Harrelson",
+    posterUrl:
+      "https://images-na.ssl-images-amazon.com/images/M/MV5BMjA5Njk3MjM4OV5BMl5BanBnXkFtZTcwMTc5MTE1MQ@@._V1_SX300.jpg"
+  },
+  {
+    id: 14,
+    title: "Planet 51",
+    year: "2009",
+    badge: "Authentic news",
+    shortDescription: "Jorge Blanco, Javier Abad, Marcos Martínez",
+    actors: "Jessica Biel, John Cleese, Gary Oldman, Dwayne Johnson",
+    posterUrl:
+      "http://ia.media-imdb.com/images/M/MV5BMTUyOTAyNTA5Ml5BMl5BanBnXkFtZTcwODU2OTM0Mg@@._V1_SX300.jpg"
+  },
+  {
+    id: 15,
+    title: "Looper",
+    year: "2012",
+    badge: "Authentic news",
+    shortDescription: "Rian Johnson",
+    actors: "Joseph Gordon-Levitt, Bruce Willis, Emily Blunt, Paul Dano",
+    posterUrl:
+      "http://ia.media-imdb.com/images/M/MV5BMTY3NTY0MjEwNV5BMl5BanBnXkFtZTcwNTE3NDA1OA@@._V1_SX300.jpg"
+  },
+  {
+    id: 16,
+    title: "Corpse Bride",
+    year: "2005",
+    badge: "Authentic news",
+    shortDescription: "Tim Burton, Mike Johnson",
+    actors: "Johnny Depp, Helena Bonham Carter, Emily Watson, Tracey Ullman",
+    posterUrl:
+      "http://ia.media-imdb.com/images/M/MV5BMTk1MTY1NjU4MF5BMl5BanBnXkFtZTcwNjIzMTEzMw@@._V1_SX300.jpg"
+  },
+  {
+    id: 17,
+    title: "The Third Man",
+    year: "1949",
+    badge: "Authentic news",
+    shortDescription: "Carol Reed",
+    actors: "Joseph Cotten, Alida Valli, Orson Welles, Trevor Howard",
+    posterUrl:
+      "https://images-na.ssl-images-amazon.com/images/M/MV5BMjMwNzMzMTQ0Ml5BMl5BanBnXkFtZTgwNjExMzUwNjE@._V1_SX300.jpg"
+  },
+  {
+    id: 18,
+    title: "The Beach",
+    year: "2000",
+    badge: "Authentic news",
+    shortDescription: "Daniel York",
+    actors:
+      "Leonardo DiCaprio, Daniel York, Patcharawan Patarakijjanon, Virginie Ledoyen",
+    posterUrl:
+      "https://images-na.ssl-images-amazon.com/images/M/MV5BN2ViYTFiZmUtOTIxZi00YzIxLWEyMzUtYjQwZGNjMjNhY2IwXkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_SX300.jpg"
+  },
+  {
+    id: 19,
+    title: "Scarface",
+    year: "1983",
+    badge: "Authentic news",
+    shortDescription: "Chloe Webb",
+    actors:
+      "Al Pacino, Steven Bauer, Michelle Pfeiffer, Mary Elizabeth Mastrantonio",
+    posterUrl:
+      "https://images-na.ssl-images-amazon.com/images/M/MV5BMjAzOTM4MzEwNl5BMl5BanBnXkFtZTgwMzU1OTc1MDE@._V1_SX300.jpg"
+  },
+  {
+    id: 20,
+    title: "Sid and Nancy",
+    year: "1986",
+    badge: "Authentic news",
+    shortDescription: "Alex Cox",
+    actors: "Gary Oldman, Chloe Webb, David Hayman, Debby Bishop",
+    posterUrl:
+      "https://images-na.ssl-images-amazon.com/images/M/MV5BMjExNjA5NzY4M15BMl5BanBnXkFtZTcwNjQ2NzI5NA@@._V1_SX300.jpg"
+  },
+  {
+    id: 21,
+    title: "Black Swan",
+    year: "2010",
+    badge: "Authentic news",
+    shortDescription: "Darren Aronofsky",
+    actors: "Natalie Portman, Mila Kunis, Vincent Cassel, Barbara Hershey",
+    posterUrl:
+      "https://images-na.ssl-images-amazon.com/images/M/MV5BNzY2NzI4OTE5MF5BMl5BanBnXkFtZTcwMjMyNDY4Mw@@._V1_SX300.jpg"
+  },
+  {
+    id: 22,
+    title: "Inception",
+    year: "2010",
+    badge: "Authentic news",
+    shortDescription: "Christopher Nolan",
+    actors: "Leonardo DiCaprio, Joseph Gordon-Levitt, Ellen Page, Tom Hardy",
+    posterUrl:
+      "https://images-na.ssl-images-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg"
+  },
+  {
+    id: 23,
+    title: "The Deer Hunter",
+    year: "1978",
+    badge: "Authentic news",
+    shortDescription: "Michael Cimino",
+    actors: "Robert De Niro, John Cazale, John Savage, Christopher Walken",
+    posterUrl:
+      "https://images-na.ssl-images-amazon.com/images/M/MV5BMTYzYmRmZTQtYjk2NS00MDdlLTkxMDAtMTE2YTM2ZmNlMTBkXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg"
+  },
+  {
+    id: 24,
+    title: "Chasing Amy",
+    year: "1997",
+    badge: "Authentic news",
+    shortDescription: "Kevin Smith",
+    actors: "Ethan Suplee, Ben Affleck, Scott Mosier, Jason Lee",
+    posterUrl:
+      "https://images-na.ssl-images-amazon.com/images/M/MV5BZDM3MTg2MGUtZDM0MC00NzMwLWE5NjItOWFjNjA2M2I4YzgxXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg"
+  },
+  {
+    id: 25,
+    title: "Django Unchained",
+    year: "2012",
+    badge: "Authentic news",
+    shortDescription: "Quentin Tarantino",
+    actors: "Jamie Foxx, Christoph Waltz, Leonardo DiCaprio, Kerry Washington",
+    posterUrl:
+      "http://ia.media-imdb.com/images/M/MV5BMjIyNTQ5NjQ1OV5BMl5BanBnXkFtZTcwODg1MDU4OA@@._V1_SX300.jpg"
+  },
+  {
+    id: 26,
+    title: "The Silence of the Lambs",
+    year: "1991",
+    badge: "Authentic news",
+    shortDescription: "Annette Bening",
+    actors:
+      "Jodie Foster, Lawrence A. Bonney, Kasi Lemmons, Lawrence T. Wrentz",
+    posterUrl:
+      "https://images-na.ssl-images-amazon.com/images/M/MV5BMTQ2NzkzMDI4OF5BMl5BanBnXkFtZTcwMDA0NzE1NA@@._V1_SX300.jpg"
+  },
+  {
+    id: 27,
+    title: "American Beauty",
+    year: "1999",
+    badge: "Authentic news",
+    shortDescription: "Sam Mendes",
+    actors: "Kevin Spacey, Annette Bening, Thora Birch, Wes Bentley",
+    posterUrl:
+      "https://images-na.ssl-images-amazon.com/images/M/MV5BMjM4NTI5NzYyNV5BMl5BanBnXkFtZTgwNTkxNTYxMTE@._V1_SX300.jpg"
+  },
+  {
+    id: 28,
+    title: "Snatch",
+    year: "2000",
+    badge: "Authentic news",
+    shortDescription: "Guy Ritchie",
+    actors: "Benicio Del Toro, Dennis Farina, Vinnie Jones, Brad Pitt",
+    posterUrl:
+      "http://ia.media-imdb.com/images/M/MV5BMTA2NDYxOGYtYjU1Mi00Y2QzLTgxMTQtMWI1MGI0ZGQ5MmU4XkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_SX300.jpg"
+  },
+  {
+    id: 29,
+    title: "Midnight Express",
+    year: "1978",
+    badge: "Authentic news",
+    shortDescription: "Alan Parker",
+    actors: "Brad Davis, Irene Miracle, Bo Hopkins, Paolo Bonacelli",
+    posterUrl:
+      "https://images-na.ssl-images-amazon.com/images/M/MV5BMTQyMDA5MzkyOF5BMl5BanBnXkFtZTgwOTYwNTcxMTE@._V1_SX300.jpg"
+  },
+  {
+    id: 30,
+    title: "Pulp Fiction",
+    year: "1994",
+    badge: "Authentic news",
+    shortDescription: "Quentin Tarantino",
+    actors: "Tim Roth, Amanda Plummer, Laura Lovelace, John Travolta",
+    posterUrl:
+      "https://images-na.ssl-images-amazon.com/images/M/MV5BMTkxMTA5OTAzMl5BMl5BanBnXkFtZTgwNjA5MDc3NjE@._V1_SX300.jpg"
+  },
+  {
+    id: 31,
+    title: "Lock, Stock and Two Smoking Barrels",
+    year: "1998",
+    badge: "Authentic news",
+    shortDescription: "Guy Ritchie",
+    actors: "Jason Flemyng, Dexter Fletcher, Nick Moran, Jason Statham",
+    posterUrl:
+      "https://images-na.ssl-images-amazon.com/images/M/MV5BMTAyN2JmZmEtNjAyMy00NzYwLThmY2MtYWQ3OGNhNjExMmM4XkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_SX300.jpg"
+  },
+  {
+    id: 32,
+    title: "Lucky Number Slevin",
+    year: "2006",
+    badge: "Authentic news",
+    shortDescription: "Paul McGuigan",
+    actors: "Josh Hartnett, Bruce Willis, Lucy Liu, Morgan Freeman",
+    posterUrl:
+      "https://images-na.ssl-images-amazon.com/images/M/MV5BMzc1OTEwMTk4OF5BMl5BanBnXkFtZTcwMTEzMDQzMQ@@._V1_SX300.jpg"
+  },
+  {
+    id: 33,
+    title: "Rear Window",
+    year: "1954",
+    badge: "Authentic news",
+    shortDescription: "Alfred Hitchcock",
+    actors: "James Stewart, Grace Kelly, Wendell Corey, Thelma Ritter",
+    posterUrl:
+      "https://images-na.ssl-images-amazon.com/images/M/MV5BNGUxYWM3M2MtMGM3Mi00ZmRiLWE0NGQtZjE5ODI2OTJhNTU0XkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg"
+  },
+  {
+    id: 34,
+    title: "Pan's Labyrinth",
+    year: "2006",
+    badge: "Authentic news",
+    shortDescription: "Guillermo del Toro",
+    actors: "Ivana Baquero, Sergi López, Maribel Verdú, Doug Jones",
+    posterUrl: ""
+  },
+  {
+    id: 35,
+    title: "Shutter Island",
+    year: "2010",
+    badge: "Authentic news",
+    shortDescription: "Martin Scorsese",
+    actors: "Leonardo DiCaprio, Mark Ruffalo, Ben Kingsley, Max von Sydow",
+    posterUrl:
+      "https://images-na.ssl-images-amazon.com/images/M/MV5BMTMxMTIyNzMxMV5BMl5BanBnXkFtZTcwOTc4OTI3Mg@@._V1_SX300.jpg"
+  },
+  {
+    id: 36,
+    title: "Reservoir Dogs",
+    year: "1992",
+    badge: "Authentic news",
+    shortDescription: "Quentin Tarantino",
+    actors: "Harvey Keitel, Tim Roth, Michael Madsen, Chris Penn",
+    posterUrl:
+      "https://images-na.ssl-images-amazon.com/images/M/MV5BNjE5ZDJiZTQtOGE2YS00ZTc5LTk0OGUtOTg2NjdjZmVlYzE2XkEyXkFqcGdeQXVyMzM4MjM0Nzg@._V1_SX300.jpg"
+  },
+  {
+    id: 37,
+    title: "The Shining",
+    year: "1980",
+    badge: "Authentic news",
+    shortDescription: "Stanley Kubrick",
+    actors: "Jack Nicholson, Shelley Duvall, Danny Lloyd, Scatman Crothers",
+    posterUrl:
+      "http://ia.media-imdb.com/images/M/MV5BODMxMjE3NTA4Ml5BMl5BanBnXkFtZTgwNDc0NTIxMDE@._V1_SX300.jpg"
+  },
+  {
+    id: 38,
+    title: "Midnight in Paris",
+    year: "2011",
+    badge: "Fake news",
+    shortDescription: "Woody Allen",
+    actors: "Owen Wilson, Rachel McAdams, Kurt Fuller, Mimi Kennedy",
+    posterUrl:
+      "http://ia.media-imdb.com/images/M/MV5BMTM4NjY1MDQwMl5BMl5BanBnXkFtZTcwNTI3Njg3NA@@._V1_SX300.jpg"
+  },
+  {
+    id: 39,
+    title: "Les Misérables",
+    year: "2012",
+    badge: "Fake news",
+    shortDescription: "Tom Hooper",
+    actors: "Hugh Jackman, Russell Crowe, Anne Hathaway, Amanda Seyfried",
+    posterUrl:
+      "http://ia.media-imdb.com/images/M/MV5BMTQ4NDI3NDg4M15BMl5BanBnXkFtZTcwMjY5OTI1OA@@._V1_SX300.jpg"
+  },
+  {
+    id: 40,
+    title: "L.A. Confidential",
+    year: "1997",
+    badge: "Fake news",
+    shortDescription: "Curtis Hanson",
+    actors: "Kevin Spacey, Russell Crowe, Guy Pearce, James Cromwell",
+    posterUrl:
+      "https://images-na.ssl-images-amazon.com/images/M/MV5BNWEwNDhhNWUtYWMzNi00ZTNhLWFiZDAtMjBjZmJhMTU0ZTY2XkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_SX300.jpg"
+  },
+  {
+    id: 41,
+    title: "Moneyball",
+    year: "2011",
+    badge: "Fake news",
+    shortDescription: "Bennett Miller",
+    actors: "Brad Pitt, Jonah Hill, Philip Seymour Hoffman, Robin Wright",
+    posterUrl:
+      "https://images-na.ssl-images-amazon.com/images/M/MV5BMjAxOTU3Mzc1M15BMl5BanBnXkFtZTcwMzk1ODUzNg@@._V1_SX300.jpg"
+  },
+  {
+    id: 42,
+    title: "The Hangover",
+    year: "2009",
+    badge: "Fake news",
+    shortDescription: "Todd Phillips",
+    actors: "Bradley Cooper, Ed Helms, Zach Galifianakis, Justin Bartha",
+    posterUrl:
+      "https://images-na.ssl-images-amazon.com/images/M/MV5BMTU1MDA1MTYwMF5BMl5BanBnXkFtZTcwMDcxMzA1Mg@@._V1_SX300.jpg"
+  },
+  {
+    id: 43,
+    title: "The Great Beauty",
+    year: "2013",
+    badge: "Fake news",
+    shortDescription: "Paolo Sorrentino",
+    actors: "Toni Servillo, Carlo Verdone, Sabrina Ferilli, Carlo Buccirosso",
+    posterUrl:
+      "https://images-na.ssl-images-amazon.com/images/M/MV5BMTQ0ODg1OTQ2Nl5BMl5BanBnXkFtZTgwNTc2MDY1MDE@._V1_SX300.jpg"
+  },
+  {
+    id: 44,
+    title: "Gran Torino",
+    year: "2008",
+    badge: "Fake news",
+    shortDescription: "Clint Eastwood",
+    actors: "Clint Eastwood, Christopher Carley, Bee Vang, Ahney Her",
+    posterUrl:
+      "http://ia.media-imdb.com/images/M/MV5BMTQyMTczMTAxMl5BMl5BanBnXkFtZTcwOTc1ODE0Mg@@._V1_SX300.jpg"
+  },
+  {
+    id: 45,
+    title: "Mary and Max",
+    year: "2009",
+    badge: "Fake news",
+    shortDescription: "Adam Elliot",
+    actors: "Toni Collette, Philip Seymour Hoffman, Barry Humphries, Eric Bana",
+    posterUrl:
+      "https://images-na.ssl-images-amazon.com/images/M/MV5BMTQ1NDIyNTA1Nl5BMl5BanBnXkFtZTcwMjc2Njk3OA@@._V1_SX300.jpg"
+  },
+  {
+    id: 46,
+    title: "Flight",
+    year: "2012",
+    badge: "Fake news",
+    shortDescription: "Robert Zemeckis",
+    actors:
+      "Nadine Velazquez, Denzel Washington, Carter Cabassa, Adam C. Edwards",
+    posterUrl:
+      "https://images-na.ssl-images-amazon.com/images/M/MV5BMTUxMjI1OTMxNl5BMl5BanBnXkFtZTcwNjc3NTY1OA@@._V1_SX300.jpg"
+  },
+  {
+    id: 47,
+    title: "One Flew Over the Cuckoo's Nest",
+    year: "1975",
+    badge: "Fake news",
+    shortDescription: "Milos Forman",
+    actors: "Michael Berryman, Peter Brocco, Dean R. Brooks, Alonzo Brown",
+    posterUrl:
+      "https://images-na.ssl-images-amazon.com/images/M/MV5BYmJkODkwOTItZThjZC00MTE0LWIxNzQtYTM3MmQwMGI1OWFiXkEyXkFqcGdeQXVyNjUwNzk3NDc@._V1_SX300.jpg"
+  },
+  {
+    id: 48,
+    title: "Requiem for a Dream",
+    year: "2000",
+    badge: "Fake news",
+    shortDescription: "Darren Aronofsky",
+    actors: "Ellen Burstyn, Jared Leto, Jennifer Connelly, Marlon Wayans",
+    posterUrl:
+      "https://images-na.ssl-images-amazon.com/images/M/MV5BMTkzODMzODYwOF5BMl5BanBnXkFtZTcwODM2NjA2NQ@@._V1_SX300.jpg"
+  },
+  {
+    id: 49,
+    title: "The Truman Show",
+    year: "1998",
+    badge: "Fake news",
+    shortDescription: "Peter Weir",
+    actors: "Jim Carrey, Laura Linney, Noah Emmerich, Natascha McElhone",
+    posterUrl:
+      "https://images-na.ssl-images-amazon.com/images/M/MV5BMDIzODcyY2EtMmY2MC00ZWVlLTgwMzAtMjQwOWUyNmJjNTYyXkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_SX300.jpg"
+  },
+  {
+    id: 50,
+    title: "The Artist",
+    year: "2011",
+    badge: "Fake news",
+    shortDescription: "Michel Hazanavicius",
+    actors: "Jean Dujardin, Bérénice Bejo, John Goodman, James Cromwell",
+    posterUrl:
+      "https://images-na.ssl-images-amazon.com/images/M/MV5BMzk0NzQxMTM0OV5BMl5BanBnXkFtZTcwMzU4MDYyNQ@@._V1_SX300.jpg"
+  },
+];
