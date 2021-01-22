1a0e5e52689cc5a89c5ac04a2c38ffbf6444d855
Mashkour Abdel Aziz <azizmashkour@gmail.com>
2021-01-22 21:50:33 +0100

    feat: create not found page

---
+++ src/components/ui/not-found/NotFound.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const NotFound =()=> {
+  {/* no need to create a style file just for 3 lines, so we use a styled component */}
+  const divStyle = {
+    textAlign: 'center',
+    fontSize: '7rem',
+    color: '#42A5F5',
+  }
+
+  return (
+    <div style={divStyle}>
+      <h1>404</h1>
+    </div>
+  );
+}
+
+export default NotFound;
