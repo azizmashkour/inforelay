b3194bf76a5da9e83f276771ba9d6c5c91395411
Mashkour Abdel Aziz <azizmashkour@gmail.com>
2021-01-22 21:49:32 +0100

    feat: create app contants variables

---
+++ src/constants/constants.js
@@ -0,0 +1,18 @@
+export const DEBUG = true;
+export const APP_ENV = 'DEV';
+export const PROTOCOL = 'http';
+export const API = {}
+
+export const CONSTANTS = {
+  LOCAL_STORAGE_LANG_KEY: 'RS_LANG_KEY',
+  LANGUAGES: [
+    {
+      key: 'en',
+      name: 'english'
+    },
+    {
+      key: 'fr',
+      name: 'french'
+    }
+  ],
+}
