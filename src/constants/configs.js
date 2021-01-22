bf057b22a0d3572bf065de599438ca34ca2a560e
Mashkour Abdel Aziz <azizmashkour@gmail.com>
2021-01-22 21:49:13 +0100

    feat: create a single file for app routes paths names

---
+++ src/constants/configs.js
@@ -0,0 +1,15 @@
+export const navigation = {
+  home: '/',
+  login: '/login',
+  not_found: '/404',
+  editor: '/editor',
+  profile: '/profile',
+  team: '/team',
+  request: '/request',
+  documentation: '/documentation',
+  dashboard: '/dashboard',
+  helpcenter: '/help-center',
+  notifications: '/notifications',
+  article_details: '/article/:articleId',
+  request_details: '/request/:requestId',
+};
