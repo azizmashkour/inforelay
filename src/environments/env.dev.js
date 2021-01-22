11eac2d0eb541751c11912520dd5ca2bd0485f3e
Mashkour Abdel Aziz <azizmashkour@gmail.com>
2021-01-22 21:46:42 +0100

    fest: create dot env file configs

---
+++ src/environments/env.dev.js
@@ -0,0 +1,11 @@
+export const environment = {
+  APP_NAME: 'Inforelay',
+  API_BASE_URL: 'https://api.tvmaze.com',
+  API_MOVIES_URL: '/search/shows',
+  API_MOVIE_URL: '/shows',
+  DEFAULT_SEARCH_MOVIE: 'actu',
+  MOVIE_PLACEHOLDER_URL: window.origin + '/article-placeholder.jpg',
+  API_BASE_URL_INF: 'https://covidrelaiapi.herokuapp.com',
+  API_NOTIFS_URL: '/api/notifications',
+  API_REQUESTS_URL: '/api/user',
+};
