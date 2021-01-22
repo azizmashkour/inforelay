d1c179d547ce0c440710b9f46f77fb6aa99ae5c6
Mashkour Abdel Aziz <azizmashkour@gmail.com>
2021-01-22 21:47:00 +0100

    feat: create footer data configs file

---
+++ src/datas/FooterDatas.js
@@ -0,0 +1,20 @@
+const FooterData = [
+  {
+    title: 'Company',
+    description: ['Team', 'Contact us'],
+  },
+  {
+    title: 'Features',
+    description: ['Articles', 'Rich editor'],
+  },
+  {
+    title: 'Resources',
+    description: ['Documentation', 'API'],
+  },
+  {
+    title: 'Legal',
+    description: ['Privacy policy', 'Terms of use'],
+  },
+];
+
+export default FooterData;
