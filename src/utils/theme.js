a4acba7857a2e98bb565af9c37c8655edc80f714
Mashkour Abdel Aziz <azizmashkour@gmail.com>
2021-01-22 21:44:24 +0100

    feat: create a separated material ui theme configs file

---
+++ src/utils/theme.js
@@ -0,0 +1,44 @@
+import { red } from '@material-ui/core/colors';
+import { createMuiTheme } from '@material-ui/core/styles';
+
+// A custom theme for this app
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#42A5F5',
+    },
+    secondary: {
+      main: '#f6bbb1',
+    },
+    error: {
+      main: red.A400,
+    },
+    background: {
+      default: '#fff',
+    },
+  },
+  typography: {
+    fontFamily: [
+      'Nunito',
+      '"Segoe UI"',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(','),
+  },
+  overrides: {
+    MuiCheckbox: {
+      colorSecondary: {
+        '&$checked': {
+          color: '#7633ff',
+        },
+      },
+    },
+  },
+});
+
+export default theme;
