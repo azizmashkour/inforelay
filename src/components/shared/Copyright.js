74cb18a31e02a0ae65eb82f6de1d498576a28464
Mashkour Abdel Aziz <azizmashkour@gmail.com>
2021-01-22 21:52:26 +0100

    feat: create copyrights component page

---
+++ src/components/shared/Copyright.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { environment as env } from '../../environments/env.dev';
+import Typography from '@material-ui/core/Typography';
+import { navigation } from '../../constants/configs';
+import Link from '@material-ui/core/Link';
+
+const Copyright =()=> {
+  const currentDate = new Date().getFullYear();
+
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" to={navigation.home}>
+        { env.APP_NAME }
+      </Link>{' '}
+      {currentDate}
+      {'.'}
+    </Typography>
+  );
+}
+
+export default Copyright;
