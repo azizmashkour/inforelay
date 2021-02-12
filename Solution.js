7d41bd3c939736ef5888483e1f060f26071d78b9
Mashkour <azizmashkour@gmail.com>
2021-02-12 13:31:44 +0100

    Create Solution.js

---
+++ Solution.js
@@ -0,0 +1,21 @@
+  const solution =(arr)=> {
+    // at the begening be initialize all
+    // at the first position of the given array
+    let k = 0;
+    let min, max = arr[0];
+    // we loop within the array
+    // to retrieve respectively the max, the min and,
+    // the position of the best position to split the array
+    for (let i = 1; i < arr.length; i++) {
+      if (arr[i] > max) {
+        max = arr[i];
+        k = i;
+      }
+      if (arr[i] < min) {
+        min = arr[i];
+      }
+    }
+    const maxDiff = Math.abs(max - min);
+    console.log(`| ${max} - ${min}| = ${maxDiff} splitted a ${k}`);
+    return maxDiff;
+  }
